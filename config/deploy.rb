21a6a234f2b18c826df72e41420e9275db10eb6e
adatu0527 <adatu0527@yahoo.ne.jp>
2019-10-19 13:22:37 +0900

    条件分岐

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,6 +55,13 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
 end
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # # デプロイ処理が終わった後、Unicornを再起動するための記述
 # after 'deploy:publishing', 'deploy:restart'
 # namespace :deploy do
