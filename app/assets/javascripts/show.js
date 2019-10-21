576ad6a6683cf5948989b193ea0b98991eaad629
aitiiwase <bicky_long_time_ago@yahoo.co.jp>
2019-10-21 16:46:48 +0900

    create-mypage(user-show)
    一通り終了
    ・サイドバーのicon
    ・一覧ボタン
    ・renderの切り出し
    はまだ行っていない

---
+++ app/assets/javascripts/show.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", function() {
+  // タブのDOM要素を取得し、変数で定義
+  let tabs = $(".main__user_tabs--menu");
+  // tabsを配列に変換する
+  tabsAry = Array.prototype.slice.call(tabs);
+
+  // クラスの切り替えをtabSwitch関数で定義
+  function tabSwitch() {
+    // 全てのactiveクラスのうち、最初の要素を削除（"[0]は、最初の要素の意味"）
+    $(".active")[0].classList.remove("active");
+
+    // クリックしたタブにactiveクラスを追加
+    this.classList.add("active");
+
+    // コンテンツの全てのshowクラスのうち、最初の要素を削除
+    $(".show")[0].classList.remove("show");
+
+    // 何番目の要素がクリックされたかを、配列tabsから要素番号を取得
+    const index = tabsAry.indexOf(this);
+
+    // クリックしたcoutentクラスにshowクラスを追加する
+    $(".contents__mainlist")[index].classList.add("show");
+  }
+
+  // タブメニューの中でクリックイベントが発生した場所を探し、下で定義したtabSwitch関数を呼び出す
+  tabsAry.forEach(function(value) {
+    value.addEventListener("click", tabSwitch);
+  });
+});
+
