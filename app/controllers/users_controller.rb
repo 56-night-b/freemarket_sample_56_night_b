8a0b1ec1c74d3da9a3c9f50d6f4260b4085fd237
aitiiwase <bicky_long_time_ago@yahoo.co.jp>
2019-11-01 18:22:00 +0900

    sign-up-controller_routes_view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,4 +32,25 @@ class UsersController < ApplicationController
 
   def reviews
   end
+
+  def sign_up1
+    @user = User.new
+  end
+
+  def sign_up2
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    @user = User.new # 新規インスタンス作成
+  end
+
+  def sign_up3
+  end
+
+  def sign_up4
+  end
+
+  def sign_up5
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,23 @@ Rails.application.routes.draw do
   get 'users/mypage/buy_products/transacted' => 'users#buy_transacted'
   get 'users/mypage/news' => 'users#news'
   get 'users/mypage/reviews' => 'users#reviews'
+ 
+  resources :users do
+    collection do
+      get 'sign_up1'
+      get 'sign_up2'
+      get 'sign_up3'
+      get 'sign_up4'
+      get 'sign_up5'
+      get 'done'
+    end
+  end
+  # get 'users/sign_up1' => 'users#sign_up1'
+  # get 'users/sign_up2' => 'users#sign_up2'
+  # get 'users/sign_up3' => 'users#sign_up3'
+  # get 'users/sign_up4' => 'users#sign_up4'
+  # get 'users/sign_up5' => 'users#sign_up5'
+
   # resources :users, only: :show
   resources :main
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
