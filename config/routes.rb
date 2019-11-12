76ae027016cd56937328dd086b9f1dc90fd1e0f2
aitiiwase <54707121+aitiiwase@users.noreply.github.com>
2019-11-12 16:36:12 +0900

    edit-routes
    コメントアウトを消しました

--- config/routes.rb
+++ config/routes.rb
@@ -22,13 +22,7 @@ Rails.application.routes.draw do
       get 'sign_up_done'
     end
   end
-  # get 'users/sign_up1' => 'users#sign_up1'
-  # get 'users/sign_up2' => 'users#sign_up2'
-  # get 'users/sign_up3' => 'users#sign_up3'
-  # get 'users/sign_up4' => 'users#sign_up4'
-  # get 'users/sign_up5' => 'users#sign_up5'
 
-  # resources :users, only: :show
   resources :main
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
